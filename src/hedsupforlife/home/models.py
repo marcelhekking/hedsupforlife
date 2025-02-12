dab3000ad02e858671b37cb3a97adf650dbbff8c
Marcel Hekking <marcelhekking@gmail.com>
2025-02-12 06:57:22 +0100

    Models mostly in place

--- src/hedsupforlife/home/models.py
+++ src/hedsupforlife/home/models.py
@@ -1,19 +1,37 @@
 from django.db import models
-from wagtail.admin.panels import FieldPanel
-from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.fields import StreamField
+from wagtail.search import index
+
+from hedsupforlife.utils.blocks import InternalLinkBlock, StoryBlock
+from hedsupforlife.utils.models import BasePage
 
 
-class HomePage(Page):
-    hoofd_pagina_afbeelding = models.ForeignKey(
-        "wagtailimages.Image",
-        null=True,
+class HomePage(BasePage):
+    template = "pages/home_page.html"
+    introduction = models.TextField(blank=True)
+    hero_cta = StreamField(
+        [("link", InternalLinkBlock())],
         blank=True,
-        on_delete=models.SET_NULL,
-        related_name="+",
+        min_num=0,
+        max_num=1,
+        use_json_field=True,
     )
-    hoofdpagina_welkom_tekst = models.TextField()
+    body = StreamField(StoryBlock(), use_json_field=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel("hoofd_pagina_afbeelding"),
-        FieldPanel("hoofdpagina_welkom_tekst"),
+    search_fields = BasePage.search_fields + [index.SearchField("introduction")]
+
+    content_panels = BasePage.content_panels + [
+        FieldPanel("introduction"),
+        FieldPanel("hero_cta"),
+        FieldPanel("body"),
+        MultiFieldPanel(
+            [
+                InlinePanel(
+                    "page_related_pages",
+                    label="Pages",
+                    max_num=12,
+                ),
+            ],
+        ),
     ]
